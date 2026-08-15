97348ee6a2b9242c02c83a3b7a29db2a2438962e
Tre2k3 <137811217+Tre2k3@users.noreply.github.com>
2026-08-15 09:40:54 -0500

    Enable 2.5D sprite pack V4 at game boot

--- src/game/materials.ts
+++ src/game/materials.ts
@@ -2,6 +2,7 @@ import * as THREE from "three";
 import "./runtimeFixesV2";
 import "./runtimeWatchdog";
 import "./runtimeLogisticsV3";
+import "./runtimeSpritePackV4";
 
 export const MAT_URLS = {
   asphalt: "/game/materials/01_asphalt_basecolor.png",
