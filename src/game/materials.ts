7159fbbb1f60c1718ca106edddaf60632882640a
Tre2k3 <137811217+Tre2k3@users.noreply.github.com>
2026-08-15 07:32:50 -0500

    Wire logistics v3 runtime

--- src/game/materials.ts
+++ src/game/materials.ts
@@ -1,6 +1,7 @@
 import * as THREE from "three";
 import "./runtimeFixesV2";
 import "./runtimeWatchdog";
+import "./runtimeLogisticsV3";
 
 export const MAT_URLS = {
   asphalt: "/game/materials/01_asphalt_basecolor.png",
