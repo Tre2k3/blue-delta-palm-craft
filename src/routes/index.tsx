a29d83fd6a57e0dd13ed90a2db20708333832562
Tre2k3 <137811217+Tre2k3@users.noreply.github.com>
2026-08-17 13:39:50 -0500

    Retain and install gameplay integrity guards in production

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,6 +1,8 @@
 import { createFileRoute } from "@tanstack/react-router";
-import "../game/gameplayIntegrity";
 import { GameApp } from "../game/GameApp";
+import { installGameplayIntegrity } from "../game/gameplayIntegrity";
+
+installGameplayIntegrity();
 
 export const Route = createFileRoute("/")({
   component: GameApp,
