69cf6be426a2add6b494b741040c01df4f11ea0e
Tre2k3 <137811217+Tre2k3@users.noreply.github.com>
2026-08-17 14:11:27 -0500

    Install Memphis riverfront and world hazard passes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,11 +3,15 @@ import { installCourtWorldIntegrity } from "../game/courtWorldIntegrity";
 import { GameApp } from "../game/GameApp";
 import { installGameplayIntegrity } from "../game/gameplayIntegrity";
 import { installInteriorCollisionPass } from "../game/interiorCollisionPass";
+import { installMemphisEnvironmentPass } from "../game/memphisEnvironmentPass";
 import { installVehicleVisualPass } from "../game/vehicleVisualPass";
+import { installWorldHazardPass } from "../game/worldHazardPass";
 
 installGameplayIntegrity();
 installInteriorCollisionPass();
+installWorldHazardPass();
 installCourtWorldIntegrity();
+installMemphisEnvironmentPass();
 installVehicleVisualPass();
 
 export const Route = createFileRoute("/")({
