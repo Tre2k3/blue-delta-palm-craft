6eeec5ed82ed80eaecae196a48ec2f531db2e803
Tre2k3 <137811217+Tre2k3@users.noreply.github.com>
2026-08-17 14:09:04 -0500

    Install physical furniture collisions in enterable interiors

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,9 +2,11 @@ import { createFileRoute } from "@tanstack/react-router";
 import { installCourtWorldIntegrity } from "../game/courtWorldIntegrity";
 import { GameApp } from "../game/GameApp";
 import { installGameplayIntegrity } from "../game/gameplayIntegrity";
+import { installInteriorCollisionPass } from "../game/interiorCollisionPass";
 import { installVehicleVisualPass } from "../game/vehicleVisualPass";
 
 installGameplayIntegrity();
+installInteriorCollisionPass();
 installCourtWorldIntegrity();
 installVehicleVisualPass();
 
