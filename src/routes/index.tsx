1f930270233b5ae9ce54a740b87b0c6d3b08968e
Tre2k3 <137811217+Tre2k3@users.noreply.github.com>
2026-08-17 13:59:19 -0500

    Install 3D vehicle visual pass

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,9 +2,11 @@ import { createFileRoute } from "@tanstack/react-router";
 import { installCourtWorldIntegrity } from "../game/courtWorldIntegrity";
 import { GameApp } from "../game/GameApp";
 import { installGameplayIntegrity } from "../game/gameplayIntegrity";
+import { installVehicleVisualPass } from "../game/vehicleVisualPass";
 
 installGameplayIntegrity();
 installCourtWorldIntegrity();
+installVehicleVisualPass();
 
 export const Route = createFileRoute("/")({
   component: GameApp,
