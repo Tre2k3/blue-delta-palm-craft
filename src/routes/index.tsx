fe5d81eabd99c0bf2baf6bfb5d3a28ef9a4c4820
Tre2k3 <137811217+Tre2k3@users.noreply.github.com>
2026-08-17 13:34:03 -0500

    Load gameplay integrity guards before starting the game

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,4 +1,5 @@
 import { createFileRoute } from "@tanstack/react-router";
+import "../game/gameplayIntegrity";
 import { GameApp } from "../game/GameApp";
 
 export const Route = createFileRoute("/")({
