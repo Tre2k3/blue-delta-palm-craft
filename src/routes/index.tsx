e6cee14c98f7547587a960fcd5b5f89e382f4c1c
Tre2k3 <137811217+Tre2k3@users.noreply.github.com>
2026-08-17 13:56:18 -0500

    Install court traffic protection before game boot

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,8 +1,10 @@
 import { createFileRoute } from "@tanstack/react-router";
+import { installCourtWorldIntegrity } from "../game/courtWorldIntegrity";
 import { GameApp } from "../game/GameApp";
 import { installGameplayIntegrity } from "../game/gameplayIntegrity";
 
 installGameplayIntegrity();
+installCourtWorldIntegrity();
 
 export const Route = createFileRoute("/")({
   component: GameApp,
