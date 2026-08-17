33fad518c9e858b863de5d41bd2a4383101a318d
Tre2k3 <137811217+Tre2k3@users.noreply.github.com>
2026-08-17 14:18:25 -0500

    Install apartment layout correction before game boot

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,4 +1,5 @@
 import { createFileRoute } from "@tanstack/react-router";
+import { installApartmentLayoutPass } from "../game/apartmentLayoutPass";
 import { installCourtWorldIntegrity } from "../game/courtWorldIntegrity";
 import { GameApp } from "../game/GameApp";
 import { installGameplayIntegrity } from "../game/gameplayIntegrity";
@@ -9,6 +10,7 @@ import { installVehicleVisualPass } from "../game/vehicleVisualPass";
 import { installWorldHazardPass } from "../game/worldHazardPass";
 
 installGameplayIntegrity();
+installApartmentLayoutPass();
 installInteriorCollisionPass();
 installWorldHazardPass();
 installCourtWorldIntegrity();
